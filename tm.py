479e5ee9285b086747a1d8719fb6735c9a6d8a56
malvine <malvine.logina@gmail.com>
2016-10-29 18:06:43 +0100

    tm

--- tm.py
+++ tm.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 
 def getdata():
@@ -6,7 +7,27 @@ def getdata():
         d = json.load(json_data)
         return d
 
+
+def getmap():
+    numbers1 = []
+    numbers2 = []
+    numbers3 = []
+    for num in range(1, 101):
+        if (num % 3 == 0):
+            numbers3.append(num)
+        elif (num % 2 == 0):
+            numbers2.append(num)
+        else:
+            numbers1.append(num)
+    map = []
+    map.append(numbers1)
+    map.append(numbers2)
+    map.append(numbers3)
+    return map
+
+
 data = getdata()
+map = getmap()
 
 
 def get_by_name(name):
@@ -39,5 +60,9 @@ def get_id_by_name(name):
         if (key == name):
             valueint = int(value)
             return valueint
+
+def get_any_attack(multiplier):
+    global map
+    return random.choice(map[multiplier-1])
 
 
