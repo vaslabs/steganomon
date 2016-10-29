fb73202b8bd1d71b54615f1ee2c3c7f6a25144b2
Vasilis Nicolaou <vaslabs@yahoo.gr>
2016-10-30 00:18:38 +0100

    Use stdin to pipe things

--- poke_utils.py
+++ poke_utils.py
@@ -1,9 +1,10 @@
 import json
+import sys
+
 
 def read_data():
-	data_file = open('my_secret_message.dat')
-	data = json.load(data_file)
-	data_file.close()
+	#data_file = open('my_secret_message.dat')
+	data = json.load(sys.stdin)
 	edges = []
 	letterEdges = []
 	for letter_obj in data:
@@ -14,14 +15,10 @@ def read_data():
 	return edges, letterEdges
 
 def read_story():
-	story_file = open('story.json')
-	data = json.load(story_file)
-	story_file.close()
+	data = json.load(sys.stdin)
 	return data
 
 def read_story_dec():
-	story_file = open('story_dec.json')
-	data = json.load(story_file)
-	story_file.close()
+	data = json.load(sys.stdin)
 	return data
 
